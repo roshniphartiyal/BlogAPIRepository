cd854ab6e62bc44654e477ca3ee7f3d8dcf3e610
ROSHNI PHARTIYAL <r30081996@gmail.com>
2021-01-16 19:22:21 +0530

    Add project files.

---
+++ BlogAPI/Authentication/BasicAuthentication.cs
@@ -0,0 +1,68 @@
+﻿using BlogDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http;
+
+
+namespace Authentication
+{
+    public class BasicAuthentication : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            try
+            {
+                if (actionContext.Request.Headers.Authorization != null)
+                {
+
+                    var authToken = actionContext.Request.Headers.Authorization.Parameter;
+                    var decoAuthToken = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+                    var UserNameAndPassword = decoAuthToken.Split(':');
+                    if (IsAuthorizedUser(UserNameAndPassword[0], UserNameAndPassword[1]))
+                    {
+                        Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(UserNameAndPassword[0]), null);
+                    }
+                    else
+                    {
+                        actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                    }
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
+        }
+        public static bool IsAuthorizedUser(string Username, string Password)
+        {
+            using (BLogDatabaseEntities context = new BLogDatabaseEntities())
+            {
+                var entity = context.UserTables.FirstOrDefault(x => x.LoginName == Username && x.Password == Password);
+                if (entity == null)
+                {
+                    ErrorLog eo = new ErrorLog() {ErrorMessage="Invalid Username or Password. For Username - "+Username,Severity="high"};
+                    context.ErrorLogs.Add(eo);
+                    context.SaveChanges();
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+        }
+    }
+}
---
+++ BlogAPI/Controllers/BlogsController.cs
@@ -0,0 +1,141 @@
+﻿
+using Authentication;
+using BlogDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace BlogAPI.Controllers
+{
+
+	[BasicAuthentication]
+	public class BlogsController : ApiController
+    {
+		[HttpGet]
+		public IEnumerable<BlogTable> GetAllBlogs() {
+			
+				using (BLogDatabaseEntities context = new BLogDatabaseEntities())
+				{
+					return context.BlogTables.ToList();
+				}
+			
+        }
+
+		
+		[HttpPost]
+		public HttpResponseMessage PostNewBlog([FromBody] BlogTable obj)
+		{
+			using (BLogDatabaseEntities context = new BLogDatabaseEntities())
+			{
+				try
+				{
+
+
+					context.BlogTables.Add(obj);
+					context.SaveChanges();
+					return Request.CreateResponse(HttpStatusCode.Created, "Added Blog_ID : " + obj.BlogId + " Blog_Title : " + obj.BlogTitle);
+
+
+				}
+				catch (Exception ex)
+				{
+
+					ErrorLog eo = new ErrorLog() { ErrorMessage = "Adding Blog Failed with error -  " + ex, Severity = "low" };
+					context.ErrorLogs.Add(eo);
+					context.SaveChanges();
+
+					return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Please provide blog details to add.");
+				}
+			}
+		}
+
+		
+		[HttpDelete]
+		public HttpResponseMessage DeleteBlog(int BlogId)
+		{
+			using (BLogDatabaseEntities context = new BLogDatabaseEntities())
+			{
+				try
+				{
+
+
+					var entity = context.BlogTables.FirstOrDefault(x => x.BlogId == BlogId);
+					if (entity == null)
+					{
+						ErrorLog eo = new ErrorLog() { ErrorMessage = "Requested Blog_id " + BlogId + " not found!", Severity = "medium" };
+						context.ErrorLogs.Add(eo);
+						context.SaveChanges();
+						return Request.CreateResponse(HttpStatusCode.NotFound, "Blog_ID : " + BlogId + "Not Found");
+					}
+					else
+					{
+						context.BlogTables.Remove(entity);
+						context.SaveChanges();
+						return Request.CreateResponse(HttpStatusCode.OK, "Blog_ID : " + BlogId + " Deleted");
+					}
+
+
+
+				}
+				catch (Exception ex)
+				{
+					
+						ErrorLog eo = new ErrorLog() { ErrorMessage = "Deleting Blog Failed with error -  " + ex, Severity = "low" };
+						context.ErrorLogs.Add(eo);
+						context.SaveChanges();
+					
+					return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+				}
+			}
+
+		}
+
+		
+		[HttpPut]
+		public HttpResponseMessage PutUpdateBlog([FromBody] BlogTable obj)
+		{
+			using (BLogDatabaseEntities context = new BLogDatabaseEntities())
+			{
+				try
+				{
+
+
+					var entity = context.BlogTables.FirstOrDefault(x => x.BlogId == obj.BlogId);
+					if (entity == null)
+					{
+						ErrorLog eo = new ErrorLog() { ErrorMessage = "Edit - Requested Blog_id " + obj.BlogId + " not found!", Severity = "medium" };
+						context.ErrorLogs.Add(eo);
+						context.SaveChanges();
+						return Request.CreateResponse(HttpStatusCode.NotFound, "Blog_ID : " + obj.BlogId + " Not Found");
+					}
+					else
+					{
+						entity.BlogTitle = obj.BlogTitle;
+						entity.Content = obj.Content;
+						entity.LastModified = DateTime.Now;
+						context.SaveChanges();
+						return Request.CreateResponse(HttpStatusCode.OK, "Blog_ID : " + obj.BlogId + " Updated");
+					}
+
+
+
+				}
+				catch (Exception ex)
+				{
+
+					ErrorLog eo = new ErrorLog() { ErrorMessage = "Editing Blog Failed with error -  " + ex, Severity = "low" };
+					context.ErrorLogs.Add(eo);
+					context.SaveChanges();
+
+					return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+				}
+			}
+		}
+
+
+	}
+}
